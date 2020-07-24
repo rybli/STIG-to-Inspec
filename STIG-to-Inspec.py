efde88c8b04e513e5fd3120f57e3e3c09467d9f4
rybli <41767605+rybli@users.noreply.github.com>
2020-07-24 11:58:29 -0400

    Removed utf-8 encoding
    UTF-8 encoding ran into error trying to write bytes instead of str

--- STIG-to-Inspec.py
+++ STIG-to-Inspec.py
@@ -32,28 +32,32 @@ template = '# encoding: utf-8\n' \
            '\n' \
            'end'
 
+# TODO Get category and set the severity
+# CATI = High (0.7 Impact)
+# CATII = Medium (0.5 Impact)
+# CATIII = Low (0.3 Impact)
+
 tag_re = re.compile(r'<[^>]+>')
 re_tag = re.compile(r'Satisfies.+')
 
 for vid in root.findall('{http://checklists.nist.gov/xccdf/1.1}Group'):
-    VID = vid.get('id').encode('utf-8')
-    # pass
+    VID = vid.get('id')
     for child in vid:
         for title in child.findall('{http://checklists.nist.gov/xccdf/1.1}title'):
-            TITLE = title.text.encode('utf-8')
+            TITLE = title.text
             # pass
         for description in child.findall('{http://checklists.nist.gov/xccdf/1.1}description'):
             desc = tag_re.sub('', description.text)
-            DESCRIPTION = re_tag.sub('', desc).encode('utf-8')  # Regex removes broken style tags and not useful data.
+            DESCRIPTION = re_tag.sub('', desc)  # Regex removes broken style tags and not useful data.
             # pass
         for fixtext in child.findall('{http://checklists.nist.gov/xccdf/1.1}fixtext'):
-            FIXTEXT = fixtext.text.encode('utf-8')
+            FIXTEXT = fixtext.text
             # pass
         for morechild in child:
             for checktext in morechild.findall('{http://checklists.nist.gov/xccdf/1.1}check-content'):
-                CHECKTEXT = checktext.text.encode('utf-8')
+                CHECKTEXT = checktext.text
                 # pass
     inspecfile = open(VID + '.rb', "w")
     inspecfile.write(template.format(VID, TITLE, DESCRIPTION, CHECKTEXT.replace('"', "'"), FIXTEXT.replace('"', "'")))
     inspecfile.close()
-
+    # print(template.format(VID, TITLE, DESCRIPTION, CHECKTEXT.replace('"', "'"), FIXTEXT.replace('"', "'")))
